aa5dbb59adc458e5f13f74bea86604ef6b5cc59b
Caelin Bryant <caelinbryant@gmail.com>
2026-06-15 23:26:25 -0400

    remove comment

--- lib/utils.tsx
+++ lib/utils.tsx
@@ -2,6 +2,5 @@ import { clsx, type ClassValue } from 'clsx';
 import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs: ClassValue[]) {
-  // test
   return twMerge(clsx(inputs));
 }
